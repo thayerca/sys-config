efd7aec4da481ed055c4de9b0ac7d1734592eb5e
Casey Thayer <cthayer@zelusanalytics.com>
2024-05-09 15:53:31 -0700

    update tmux

--- zelus-m1/nvim/.config/nvim/lua/cthayer/core/keymaps.lua
+++ zelus-m1/nvim/.config/nvim/lua/cthayer/core/keymaps.lua
@@ -6,6 +6,12 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
+-- navigate vim panes better
+keymap.set("n", "<C-k>", ":wincmd k<CR>")
+keymap.set("n", "<C-j>", ":wincmd j<CR>")
+keymap.set("n", "<C-h>", ":wincmd h<CR>")
+keymap.set("n", "<C-l>", ":wincmd l<CR>")
+
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
