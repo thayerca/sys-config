27f92fa5bf834f644731689897232caa07da31ad
Casey Thayer <cthayer@zelusanalytics.com>
2024-05-09 12:57:14 -0700

    remove existing config

--- zelus-m1/nvim/.config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-require("cthayer.lazy")
-require("cthayer.mason_tools")
-require("theme.kanagawa")
---vim.cmd("source $HOME/.config/nvim/lua/old_config.vim")
-require("cthayer.set")
-require("cthayer.remap")
-
-vim.g.python3_host_prog = '~/.config/nvim/py3nvim/bin/python'
